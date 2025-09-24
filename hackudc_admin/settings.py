54c4dc79b57afe687c27133b02acc616da250eb2
Appuchia <appuchia@appu.ltd>
2025-09-24 23:11:50 +0200

    fix: Staticfiles en DEBUG

--- hackudc_admin/settings.py
+++ hackudc_admin/settings.py
@@ -130,7 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
 
 # Media files (User uploaded content)
 MEDIA_ROOT = BASE_DIR / "media"
