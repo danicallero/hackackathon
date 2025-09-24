9750f77ce791771425cc2b5cc9efe16de5913d6f
Appu <appuchia@appu.ltd>
2025-09-24 23:21:27 +0200

    feat: Login y logout gestión (#15)
    Resuelve #11

--- hackudc/urls.py
+++ hackudc/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from hackudc import views
@@ -8,4 +9,14 @@ urlpatterns = [
     path("gestion/registro", views.alta, name="alta"),
     path("gestion/pases", views.pases, name="pases"),
     path("gestion/presencia", views.presencia, name="presencia"),
+]
+
+
+urlpatterns += [
+    path(
+        "login",
+        LoginView.as_view(template_name="login.html", next_page="gestion"),
+        name="login",
+    ),
+    path("logout", LogoutView.as_view(next_page="login"), name="logout"),
 ]
