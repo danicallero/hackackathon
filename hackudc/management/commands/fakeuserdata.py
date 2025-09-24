71297c77950095ccf1cdf1e0ee366e7cf61d5e75
Iago <101663529+delthia@users.noreply.github.com>
2025-09-24 15:27:42 +0200

    Feat/fakekeuserdata (#14)
    * feat: Comando para crear datos de prueba
    
    * feat: STDOUT en vez de print

--- hackudc/management/commands/fakeuserdata.py
+++ hackudc/management/commands/fakeuserdata.py
@@ -10,28 +10,32 @@ class Command(BaseCommand):
     help = "Crea datos falsos de participantes de prueba"
 
     def add_arguments(self, parser):
-        parser.add_argument("cantidad", help="Cantidad de participantes a crear", type=int, default=100)
+        parser.add_argument(
+            "cantidad", help="Cantidad de participantes a crear", type=int, default=100
+        )
 
     def handle(self, *args, **options):
         print(f"Se crearán {options['cantidad']} participantes")
-        fake = Faker('es_ES')
+        self.stdout.write(
+            self.style.SUCCESS(f"Se crearán {options['cantidad']} participantes")
+        )
+        fake = Faker("es_ES")
 
-        for _ in range(options['cantidad']):
+        for _ in range(options["cantidad"]):
             participante = Participante(
                 correo=fake.email(),
                 nombre=fake.name(),
-                dni=fake.cif(),     # Lo más parecido a un DNI que hay en faker...
-                genero=choice(['Hombre', 'Mujer', 'Otro']),
+                dni=fake.cif(),  # Lo más parecido a un DNI que hay en faker...
+                genero=choice(["Hombre", "Mujer", "Otro"]),
                 telefono=fake.phone_number(),
                 ano_nacimiento=fake.year(),
-                nombre_estudio=choice(['GCED', 'GEI', 'MUEI', 'MUNICS']),
-                centro_estudio=choice(['FIC', 'USC', 'UVigo', 'Otro']),
+                nombre_estudio=choice(["GCED", "GEI", "MUEI", "MUNICS"]),
+                centro_estudio=choice(["FIC", "USC", "UVigo", "Otro"]),
                 curso=randint(1, 4),
                 ciudad=fake.city(),
                 quiere_creditos=choice([True, False]),
-                talla_camiseta=choice(['S', 'M', 'L', 'XL']),
+                talla_camiseta=choice(["S", "M", "L", "XL"]),
                 compartir_cv=choice([True, False]),
             )
 
             Participante.save(participante)
-
